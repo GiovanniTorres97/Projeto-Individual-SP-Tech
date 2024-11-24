f6fccf293e8ea93d5a90597c0478ed69241495d6
GiovanniTorres97 <giovanni.amante@sptech.school>
2024-11-23 22:19:36 -0300

    implementação da API funcionando

---
+++ Site/src/controllers/usuarioController.js
@@ -0,0 +1,78 @@
+var usuarioModel = require("../models/usuarioModel");
+
+function autenticar(req, res) {
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+
+    // Verificação dos campos vazios ou undefined
+    if (email == undefined || email == "") {
+        res.status(400).send("Seu email está undefined ou vazio!");
+    } else if (senha == undefined || senha == "") {
+        res.status(400).send("Sua senha está undefined ou vazia!");
+    } else {
+        // Chama a função de autenticação no modelo
+        usuarioModel.autenticar(email, senha)
+            .then(function (resultadoAutenticar) {
+                console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+                console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
+
+                if (resultadoAutenticar.length == 1) {
+                    res.json({
+                        email: resultadoAutenticar[0].email,
+                        nome: resultadoAutenticar[0].nome,
+                        id: resultadoAutenticar[0].id,
+                        cpf: resultadoAutenticar[0].cpf
+                    });
+                } else if (resultadoAutenticar.length == 0) {
+                    // Se não encontrar, retorna erro
+                    res.status(403).send("Email e/ou senha inválido(s)");
+                } else {
+                    // Se encontrar mais de um usuário, retorna erro
+                    res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+                }
+            }).catch(function (erro) {
+                console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            });
+    }
+}
+
+function cadastrar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var cpf = req.body.cpfServer;
+    var nome = req.body.nomeServer;
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+
+    // Faça as validações dos valores
+    if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    } else {
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.cadastrar(cpf, nome, email, senha)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+module.exports = {
+    autenticar,
+    cadastrar
+}
---
+++ Site/src/models/usuarioModel.js
@@ -0,0 +1,27 @@
+var database = require("../database/config");
+
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function autenticar(): ", email, senha);
+    var instrucaoSql = `
+        SELECT idUsuario, cpf, nome, email FROM usuarios WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function cadastrar(cpf, nome, email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, cpf);
+    
+    // Inserção dos dados na tabela usuarios
+    var instrucaoSql = `
+        INSERT INTO usuarios (cpf, nome, email, senha) 
+        VALUES ('${cpf}', '${nome}', '${email}', '${senha}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    autenticar,
+    cadastrar
+};
