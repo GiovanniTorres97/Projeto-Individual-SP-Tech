dcbe18368aa860963d6d1f2df7be3b91e3a12b64
GiovanniTorres97 <giovanni.amante@sptech.school>
2024-11-24 00:55:19 -0300

    adicionado dashboard inicial

--- Site/app.js
+++ Site/app.js
@@ -22,6 +22,7 @@ var medidasRouter = require("./src/routes/medidas");
 var aquariosRouter = require("./src/routes/aquarios");
 var empresasRouter = require("./src/routes/empresas");
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -34,6 +35,8 @@ app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
 app.use("/empresas", empresasRouter);
+
+
 
 app.listen(PORTA_APP, function () {
     console.log(`
